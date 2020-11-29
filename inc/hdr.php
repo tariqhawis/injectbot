38dceddc9a1effe3913b7573046f06ea76408054
tariqhawis <prof.tariq@gmail.com>
2020-11-29 18:43:06 +0300

    new version 1.6

--- inc/hdr.php
+++ inc/hdr.php
@@ -2,22 +2,23 @@
 include("var.php");
 ?>
 <!--
- # #    #      # #####   ####  #####   ####  ##### 
- # ##   #      # #    # #    # #    # #    #   #   
- # # #  #      # #    # #    # #####  #    #   #   
- # #  # #      # #####  #    # #    # #    #   #   
- # #   ## #    # #   #  #    # #    # #    #   #   
- # #    #  ####  #    #  ####  #####   ####    #   
+  _____       _           _   ____        _   
+ |_   _|     (_)         | | |  _ \      | |  
+   | |  _ __  _  ___  ___| |_| |_) | ___ | |_ 
+   | | | '_ \| |/ _ \/ __| __|  _ < / _ \| __|
+  _| |_| | | | |  __/ (__| |_| |_) | (_) | |_ 
+ |_____|_| |_| |\___|\___|\__|____/ \___/ \__|
+            _/ |                              
+           |__/                               
 
 MIT License 2008-2020 injrobot - By TrX(TM)
 
-Advisory
+-Advisory
 All the uses of this tool should be used for authorized penetration testing and/or educational purposes only. 
 Any misuse of this software will not be the responsibility of the author or of any other collaborator. 
 Use it at your own networks and/or with the network owner's permission.
 
-Looking for a useful SQL Injection Course?
-
+-Looking for a useful SQL Injection Course?
 Contact me and ask about the SQL Injection Course, I am preparing for attackers and defenders (100% technical).
 
 -->
@@ -34,14 +35,16 @@ Contact me and ask about the SQL Injection Course, I am preparing for attackers 
 <table align="center">
      <th><div id="header" title="InjRobot">
           <div id="idover" style="font-family: monospace;white-space: pre;">
-  _____       _ _____       _           _   
- |_   _|     (_)  __ \     | |         | |  
-   | |  _ __  _| |__) |___ | |__   ___ | |_ 
-   | | | '_ \| |  _  // _ \| '_ \ / _ \| __|
-  _| |_| | | | | | \ \ (_) | |_) | (_) | |_ 
- |_____|_| |_| |_|  \_\___/|_.__/ \___/ \__|
-            _/ |                            
-           |__/                             
+
+ _____       _           _   ____        _   
+ |_   _|     (_)         | | |  _ \      | |  
+   | |  _ __  _  ___  ___| |_| |_) | ___ | |_ 
+   | | | '_ \| |/ _ \/ __| __|  _ < / _ \| __|
+  _| |_| | | | |  __/ (__| |_| |_) | (_) | |_ 
+ |_____|_| |_| |\___|\___|\__|____/ \___/ \__|
+            _/ |                              
+           |__/                               
+                     
                <span style='color:red;font-size:9px'>v.<?php echo $version;?></span>
           </div></div>
      </th>
